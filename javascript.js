4117eb2bf29bc41f34abab83422ab534277e3d26
lavanbahi1 <126147641+lavanbahi1@users.noreply.github.com>
2024-12-27 21:24:31 -0500

    Highlight project boxes when hovered over

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const p1 = document.querySelector(".project1");
+
+p1.addEventListener("mouseover", () => {
+    p1.classList.add("projectsHover");
+})
+
+p1.addEventListener("mouseleave", () => {
+    p1.classList.remove("projectsHover");
+})
+
+const p2 = document.querySelector(".project2");
+
+p2.addEventListener("mouseover", () => {
+    p2.classList.add("projectsHover");
+})
+
+p2.addEventListener("mouseleave", () => {
+    p2.classList.remove("projectsHover");
+})
